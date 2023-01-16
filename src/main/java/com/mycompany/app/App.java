9e5aa01ec098b44509d7e81cab85b71f6354256a
phyllislye76 <ling-ling_lye@keysight.com>
2023-01-16 16:29:26 +0800

    My first change in the local project folder

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("Today is my day!");
+        System.out.println("I hate Jenkins");
     }
 
     public String getMessage() {
