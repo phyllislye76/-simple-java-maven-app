461a29851d07d1a1d7a14b488f9cce417dadb5c9
phyllislye76 <ling-ling_lye@keysight.com>
2023-01-17 09:34:32 +0800

    test jenkin polling

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("I love Jenkins");
+        System.out.println("Today is 2nd of training");
     }
 
     public String getMessage() {
